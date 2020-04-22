c5959a56bea60f0421f105408031285d46306892
AndyShen105 <shenhang105@gmail.com>
2020-04-22 09:25:16 +0800

    add TrieTree

--- src/main/java/leetcode/ImplementTrie.java
+++ src/main/java/leetcode/ImplementTrie.java
@@ -65,7 +65,7 @@ public class ImplementTrie {
             }
             return node.isLeaf;
         }
-        
+
         public boolean startsWith(String prefix) {
             if (prefix == null || prefix.length() == 0) {
                 return false;
@@ -85,5 +85,21 @@ public class ImplementTrie {
             return true;
         }
     }
+
+    public static int bits2words(int numBits) {
+        return ((numBits - 1) >> 6) + 1; // I.e.: get the word-offset of the last bit and add one (make sure to use >> so 0 returns 0!)
+    }
+
+
+    public static void main(String[] args) {
+        log.info(bits2words(65));
+        int numBits = 300;
+        long[] bits = new long[bits2words(numBits)];
+        int index = 330;
+        int wordNum = index >> 6;      // div 64
+        long bitmask = 1L << index;
+        bits[wordNum] |= bitmask;
+    }
+
 }
 
