5efe8d584964090dde11153491398afb92970f61
AndyShen105 <shenhang105@gmail.com>
2020-05-13 09:11:20 +0800

    add CourseSchedule

---
+++ src/main/java/leetcode/CourseSchedule.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by hang.shen@transwarp.io on 2020/5/12.
+ */
+public class CourseSchedule {
+
+    private static final Logger log = LogManager.getLogger(CourseSchedule.class);
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        return false;
+    }
+}
+
