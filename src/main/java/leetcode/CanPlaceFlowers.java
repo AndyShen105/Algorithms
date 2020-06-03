12fa22299305cc578cb7f89bfd35ab84bc405002
AndyShen105 <shenhang105@gmail.com>
2020-06-03 09:53:10 +0800

    1. fix CanPlaceFlowers

--- src/main/java/leetcode/CanPlaceFlowers.java
+++ src/main/java/leetcode/CanPlaceFlowers.java
@@ -12,7 +12,7 @@ public class CanPlaceFlowers {
 
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         if (flowerbed.length == 1 && flowerbed[0] == 0) {
-            return n == 1;
+            return n <= 1;
         }
         for (int i = 0; flowerbed.length > 1 && i < flowerbed.length - 1; i++) {
             if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
